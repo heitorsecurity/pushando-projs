f5b4dd258fb29c2fa482c8fbf4ac481b901b0b01
heitorsecurity <contato@heitorsecurity.com.br>
2024-12-13 20:53:08 -0300

    Alterando pastas, criando pastas de projetos novos e subindo explicacao de como funciona maquina virtual e jvm de algumas linguagens.

---
+++ bff-zeroum/src/main/java/bff/bff_zeroum/BffZeroumApplication.java
@@ -0,0 +1,13 @@
+package bff.bff_zeroum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffZeroumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffZeroumApplication.class, args);
+	}
+
+}
---
+++ bff-zeroum/src/test/java/bff/bff_zeroum/BffZeroumApplicationTests.java
@@ -0,0 +1,13 @@
+package bff.bff_zeroum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffZeroumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
