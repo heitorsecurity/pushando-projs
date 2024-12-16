6b0ad2d85c0291384d63614d6872540fe2c064f2
Heitor (heitorsecurity) Ramalho <5385105+heitorsecurity@users.noreply.github.com>
2024-12-16 01:11:23 -0300

    Testando projetos .NET

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Ola a todos!");
+
+//Operadores aritmeticos
+int adicao = 5 + 3;
+int subtracao = 5 - 3;
+int multiplicacao = 5 * 3;
+int divisao = 15 / 3;
+
+int expr = (1 + 2) * 5 - 2 / 2;
+
+Console.WriteLine();
---
+++ HelloWorld/HelloWorld/Tipos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeirosPassos
+	public class Tipos
+{
+		public void ConhecendoOsTipos()
+		{
+
+			//int
+			int i = 10;
+
+			//float
+			float f = 10.9f;
+
+			//double
+			double d = 20.9d;
+
+			//decimal
+			decimal c = 20.9;
+
+			//bool
+			bool b1 = true;
+			bool b2 = false;
+
+			//char
+			char c2 = 'A';
+
+			//string
+			string s = "Thaise";
+
+		var cidade = "Blumenau";
+        var idade = 30;
+
+		idade = 55;
+
+		const string pais = "Brasil";
+
+		pais = "Austrália";
+
+		f = idade;
+		f = (float)c;
+
+		}
+}
