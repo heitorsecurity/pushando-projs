6fdba104edd72f882c1058fbf53e678c635203b0
Heitor (heitorsecurity) Ramalho <5385105+heitorsecurity@users.noreply.github.com>
2024-12-24 13:54:08 -0300

    Adicionando tratamento de erros e excecoes.

--- python-rascunhao/relogao/sinc_relogio2.py
+++ python-rascunhao/relogao/sinc_relogio2.py
@@ -2,9 +2,14 @@ import ntplib
 import time
 
 def get_ntp_time():
-    c = ntplib.NTPClient()
-    response = c.request('pool.ntp.org')
-    return time.ctime(response.tx_time)
+    try:
+        c = ntplib.NTPClient()
+        response = c.request('south-america.pool.ntp.org')
+        return time.ctime(response.tx_time)
+    except ntplib.NTPException as e:
+        return f"Erro tentando puxar o tempo do NTP: {e}"
+    except Exception as e:
+        return f"Um erro inesperado ocorreu: {e}"
 
 # Chame a funcao e printe o resultado
 print(get_ntp_time())
